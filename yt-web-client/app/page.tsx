03464c6b78d05054f2b11527f7f8653d821ea312
Craig Olson <craig.olson804@gmail.com>
2023-08-19 14:44:10 -0700

    Add navbar and assets

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
@@ -9,10 +8,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
-        
       </div>
-
-
     </main>
-  )
+  );
 }
