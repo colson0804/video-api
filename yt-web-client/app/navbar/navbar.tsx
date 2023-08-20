dc7d575c17b8c0782b2be55e175235fcf7f58967
Craig Olson <craig.olson804@gmail.com>
2023-08-20 11:47:16 -0700

    Create Navigation bar component

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,8 +1,23 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
+import { useState, useEffect } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
 
 export default function Navbar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
@@ -13,6 +28,7 @@ export default function Navbar() {
           alt="Youtube Logo"
         />
       </Link>
+      <SignIn user={user} />
     </nav>
   );
 }
---
+++ yt-web-client/app/navbar/sign-in.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { signInWithGoogle, signOut } from "../firebase/firebase";
+import { User } from "firebase/auth";
+import styles from "./sign-in.module.css";
+
+interface SignInProps {
+  user: User | null;
+}
+
+export default function SignIn({ user }: SignInProps) {
+  return (
+    <div>
+      <>
+        {user ? (
+          <button className={styles.signin} onClick={signOut}>
+            Sign Out
+          </button>
+        ) : (
+          <button className={styles.signin} onClick={signInWithGoogle}>
+            Sign In
+          </button>
+        )}
+      </>
+    </div>
+  );
+}
